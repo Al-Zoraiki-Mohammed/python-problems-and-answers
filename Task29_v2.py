d065db5cec2a25ad1d4dbb28aba20aae31a662fa
Al-Zoraiki-Mohammed <mama.zoraiki2017@gmail.com>
2023-11-29 20:35:34 +0100

    'updated'

--- Task29_v2.py
+++ Task29_v2.py
@@ -2,13 +2,14 @@ def minion_game(word):
     Kevin_scores = 0
     Stuart_scores = 0
     vowels = 'AEOUI'
+    length = len(word)
 
-    for i in range (len(word)):
+    for i in range (len(word)): 
         if  word[i] in vowels:
-            Kevin_scores += len(word[i:])
+            Kevin_scores += length - i
         else:
-            Stuart_scores += len(word[i:])
-    
+            Stuart_scores += length - i 
+
 
     if Stuart_scores > Kevin_scores:
         print(f'Stuart {Stuart_scores}')
@@ -22,6 +23,3 @@ if __name__ == '__main__':
     s = input('input a word: ')
     minion_game(s)
 
-# Note:
-# computing len(string[i:]) every time is time consuming and makes our code not efficient.
-# In Task29_v2 we will optmize the code to make it faster.
